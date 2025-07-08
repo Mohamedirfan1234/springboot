20e2f2ed57f18e514e623ac7f886efbcba774a23
Mohamedirfan1234 <152084076+Mohamedirfan1234@users.noreply.github.com>
2025-07-08 11:16:01 +0530

    Delete SpringBoot-Intern-archive-day4/demo directory

--- SpringBoot-Intern-archive-day4/demo/Day-1/src/main/java/org/example/Course.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Course {
-    public void courseDetails() {
-        System.out.println("Course details are being provided.");
-    }
-}
--- SpringBoot-Intern-archive-day4/demo/Day-1/src/main/java/org/example/Teacher.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Teacher {
-    public void teach() {
-        System.out.println("Teacher is conducting a lecture.");
-    }
-}
